fa7348cc271bc6af4b78b5fd2a56766034675b99
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-05 03:14:35 -0300

    add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,28 @@
-import Home from 'templates/Home';
+import Home, { HomeTemplateProps } from 'templates/Home';
 
-export default function Index() {
-  return <Home />;
+import bannersMock from 'components/BannerSlider/mock';
+import gamesMock from 'components/GameCardSlider/mock';
+import highlightMock from 'components/Highlight/mock';
+
+export default function Index(props: HomeTemplateProps) {
+  return <Home {...props} />;
+}
+
+// getStaticProps => gerar estático em build time
+// getServerSideProps => gerar via ssr a cada request
+// getInitialProps => gerar via ssr a cada request
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: bannersMock,
+      newGames: gamesMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gamesMock,
+      upcommingGames: gamesMock,
+      upcommingHighligth: highlightMock,
+      upcommingMoreGames: gamesMock,
+      freeGames: gamesMock,
+      freeHighligth: highlightMock,
+    },
+  };
 }
