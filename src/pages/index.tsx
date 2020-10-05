62a580944be4dac0006e7d31672918403986a239
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-05 00:13:11 -0300

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,5 @@
-import * as S from './styles';
+import Home from 'templates/Home';
 
-export default function Home() {
-  return (
-    <S.Wrapper>
-      <S.Logo
-        src="/img/logo.svg"
-        alt="Imagem de um átomo e React Avançado escrito ao lado"
-      />
-      <S.Title>Won - Games!</S.Title>
-      <S.Description>Typescript, NextJS e Styled Components</S.Description>
-      <S.Illustration
-        src="/img/hero-illustration.svg"
-        alt="Um desenvolvedor de frente para uma tela de código"
-      />
-    </S.Wrapper>
-  );
+export default function Index() {
+  return <Home />;
 }
