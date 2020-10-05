7d082bb9c8a56e687208743b65f16c3e5d40fd4e
Anderson Rodrigues <anddersonrds@gmail.com>
2020-10-04 23:39:00 -0300

    customize background won-theme in storybooks

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,22 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
 
+export const parameters = {
+  background: {
+    default: 'won-light',
+    values: [
+      {
+        name: 'won-light',
+        valuer: theme.colors.white
+      },
+      {
+        name: 'won-dark',
+        valuer: theme.colors.mainMg
+      }
+    ]
+  }
+}
+
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
@@ -10,4 +26,6 @@ export const decorators = [
     </ThemeProvider>
   ),
 ]
+
+
 
