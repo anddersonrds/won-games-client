b2d8fe7bb6be32e3ab3819e7763515e923f6710e
Anderson Rodrigues <anddersonrds@gmail.com>
2020-11-30 15:23:13 -0300

    add FormSignIn Component

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,15 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { renderWithTheme } from 'utils/tests/helpers';
 
 import FormSignIn from './index';
 
 describe('<FormSignIn />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<FormSignIn />);
+  it('should render the form', () => {
+    const { container } = renderWithTheme(<FormSignIn />);
 
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument();
     expect(
-      screen.getByRole('heading', { name: /FormSignIn/i }),
+      screen.getByRole('button', { name: /sign in now/i }),
     ).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container.parentElement).toMatchSnapshot();
+  });
+
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i }),
+    ).toBeInTheDocument();
+  });
+
+  it('should render text to sign up if already have an account', () => {
+    renderWithTheme(<FormSignIn />);
+
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument();
+    expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument();
   });
 });
