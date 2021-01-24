b2fdd42c42b90756ddc038fe9733d9a85ee55117
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-24 02:35:23 -0300

    Add Template Game

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,15 +1,65 @@
+/* eslint-disable jsx-a11y/aria-role */
 import Base from 'templates/Base';
+
+import GameInfo, { GameInfoProps } from 'components/GameInfo';
+import Gallery, { GalleryImageProps } from 'components/Gallery';
+import TextContent from 'components/TextContent';
+import GameDetails, { GameDetailsProps } from 'components/GameDetails';
+import { GameCardProps } from 'components/GameCard';
+import { HighlightProps } from 'components/Highlight';
+import Showcase from 'components/Showcase';
 
 import * as S from './styles';
 
-const Game = () => (
+export type GameTemplateProps = {
+  cover: string;
+  gameInfo: GameInfoProps;
+  gallery?: GalleryImageProps[];
+  description: string;
+  details: GameDetailsProps;
+  upcomingGames: GameCardProps[];
+  upcomingHighlight: HighlightProps;
+  recommendedGames: GameCardProps[];
+};
+
+const Game = ({
+  cover,
+  gameInfo,
+  gallery,
+  description,
+  details,
+  upcomingGames,
+  upcomingHighlight,
+  recommendedGames
+}: GameTemplateProps) => (
   <Base>
-    <S.Cover
-      src="https://images.gog-statics.com/5643a7c831df452d29005caeca24c28cdbfaa6fbea5a9556b147ee26d325fa70_bg_crop_1366x655.jpg"
-      // eslint-disable-next-line jsx-a11y/aria-role
-      role="image"
-      aria-label="cover"
-    />
+    <S.Cover src={cover} role="image" aria-label="cover" />
+
+    <S.Main>
+      <S.SectionGameInfo>
+        <GameInfo {...gameInfo} />
+      </S.SectionGameInfo>
+
+      <S.SectionGallery>
+        {!!gallery && <Gallery items={gallery} />}
+      </S.SectionGallery>
+
+      <S.SectionDescription>
+        <TextContent title="Description" content={description} />
+      </S.SectionDescription>
+
+      <S.SectionGameDetails>
+        <GameDetails {...details} />
+      </S.SectionGameDetails>
+
+      <Showcase
+        title="Upcoming"
+        games={upcomingGames}
+        highlight={upcomingHighlight}
+      />
+
+      <Showcase title="You may like these games" games={recommendedGames} />
+    </S.Main>
   </Base>
 );
 
