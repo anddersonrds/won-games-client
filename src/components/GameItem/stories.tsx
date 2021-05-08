e7eb3dffb2f4d086cbbf454701678f58ab1e9bb3
Anderson Rodrigues <anddersonrds@gmail.com>
2021-05-08 00:41:43 -0300

    Add GameItem Component

---
+++ src/components/GameItem/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import GameItem, { GameItemProps } from '.'
+
+export default {
+  title: 'GameItem',
+  component: GameItem,
+  args: {
+    img: 'https://source.unsplash.com/user/willianjusten/151x70',
+    title: 'Red Dead Remdeption 2',
+    price: 'R$ 215,00'
+  }
+} as Meta
+
+export const Default: Story<GameItemProps> = args => <GameItem {...args} />
+
+export const WithPayment: Story<GameItemProps> = args => <GameItem {...args} />
+
+WithPayment.args = {
+  downloadLink: 'https://wongames.com/downloads/anddersonrds'
+}
