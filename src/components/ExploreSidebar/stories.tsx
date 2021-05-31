bcfb2605cab0d59d9c397498259b517a0e6de666
Anderson Rodrigues <anddersonrds@gmail.com>
2021-05-30 23:20:59 -0300

    WIP

---
+++ src/components/ExploreSidebar/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import ExploreSidebar, { ExploreSidebarProps } from '.'
+
+import itemsMock from './mock'
+
+export default {
+  title: 'ExploreSidebar',
+  component: ExploreSidebar,
+  args: {
+    items: itemsMock
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<ExploreSidebarProps> = args => (
+  <ExploreSidebar {...args} />
+)
