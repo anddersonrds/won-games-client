61679cbb86db7986771f71f971553e16adb8cef1
Anderson Rodrigues <anddersonrds@gmail.com>
2021-08-17 21:55:31 -0300

    Add ExploreSideBar Componente Finished

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -1,61 +1,104 @@
+import { useState } from 'react'
+import { Close } from '@styled-icons/material-outlined/Close'
+import { FilterList } from '@styled-icons/material-outlined/FilterList'
+
+import Heading from 'components/Heading'
 import Button from 'components/Button'
 import Checkbox from 'components/Checkbox'
-import Heading from 'components/Heading'
 import Radio from 'components/Radio'
+
 import * as S from './styles'
 
 type Field = {
-  name: string
   label: string
+  name: string
 }
 
 export type ItemProps = {
   title: string
   name: string
-  type: 'checkbox' | 'radio'
+  type: string
   fields: Field[]
+}
+
+type Values = {
+  [field: string]: boolean | string
 }
 
 export type ExploreSidebarProps = {
   items: ItemProps[]
+  initialValues?: Values
+  onFilter: (values: Values) => void
 }
 
-const ExploreSidebar = ({ items }: ExploreSidebarProps) => (
-  <S.Wrapper>
-    {items.map(item => (
-      <div key={item.title}>
-        <Heading lineBottom lineColor="secondary" size="small">
-          {item.title}
-        </Heading>
+const ExploreSidebar = ({
+  items,
+  initialValues = {},
+  onFilter
+}: ExploreSidebarProps) => {
+  const [values, setValues] = useState(initialValues)
+  const [isOpen, setIsOpen] = useState(false)
 
-        {item.type === 'checkbox' &&
-          item.fields.map(field => (
-            <Checkbox
-              key={field.name}
-              name={field.name}
-              label={field.label}
-              labelFor={field.name}
-            />
-          ))}
+  const handleChange = (name: string, value: string | boolean) => {
+    setValues(state => ({ ...state, [name]: value }))
+  }
 
-        {item.type === 'radio' &&
-          item.fields.map(field => (
-            <Radio
-              key={field.name}
-              id={field.name}
-              name={item.name}
-              label={field.label}
-              labelFor={field.name}
-              value={field.name}
-            />
-          ))}
-      </div>
-    ))}
+  const handleFilter = () => {
+    onFilter(values)
+    setIsOpen(false)
+  }
 
-    <Button fullWidth size="medium">
-      Filter
-    </Button>
-  </S.Wrapper>
-)
+  return (
+    <S.Wrapper isOpen={isOpen}>
+      <S.Overlay aria-hidden={isOpen} />
+      <S.IconWrapper>
+        <FilterList aria-label="open filters" onClick={() => setIsOpen(true)} />
+        <Close aria-label="close filters" onClick={() => setIsOpen(false)} />
+      </S.IconWrapper>
+
+      <S.Content>
+        {items.map(item => (
+          <S.Items key={item.title}>
+            <Heading lineBottom lineColor="secondary" size="small">
+              {item.title}
+            </Heading>
+
+            {item.type === 'checkbox' &&
+              item.fields.map(field => (
+                <Checkbox
+                  key={field.name}
+                  name={field.name}
+                  label={field.label}
+                  labelFor={field.name}
+                  isChecked={!!values[field.name]}
+                  onCheck={value => handleChange(field.name, value)}
+                />
+              ))}
+
+            {item.type === 'radio' &&
+              item.fields.map(field => (
+                <Radio
+                  key={field.name}
+                  id={field.name}
+                  value={field.name}
+                  name={item.name}
+                  label={field.label}
+                  labelFor={field.name}
+                  defaultChecked={field.name === values[item.name]}
+                  onChange={() => handleChange(item.name, field.name)}
+                />
+              ))}
+          </S.Items>
+        ))}
+      </S.Content>
+
+      <S.Footer>
+        <Button fullWidth size="medium" onClick={handleFilter}>
+          Filter
+        </Button>
+      </S.Footer>
+    </S.Wrapper>
+  )
+}
 
 export default ExploreSidebar
